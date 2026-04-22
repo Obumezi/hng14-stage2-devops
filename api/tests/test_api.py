7166f2e013db0707ebe601d8a9ee8d8a32ccaf5a
Kelvin Eziamaka <Kelvin.Eziamaka@wemabank.com>
2026-04-22 14:01:22 +0100

    updated test_api

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,13 +1,16 @@
 from unittest.mock import patch
 
+
 def test_health():
     assert True
+
 
 @patch("api.main.r")
 def test_create_job(mock_redis):
     mock_redis.lpush.return_value = True
     mock_redis.hset.return_value = True
     assert mock_redis.lpush("jobs", "123")
+
 
 @patch("api.main.r")
 def test_get_job(mock_redis):
